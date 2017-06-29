57481447c62292eceba9a602c675fe0235f0e28c
Brad McDavid <brad@wsol.com>
2017-06-29 16:33:19 -0400

    attempting unit tests for older frameworks.

---
+++ tests/DotNetStarter.Framework.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DotNetStarter.Framework.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetStarter.Framework.Tests")]
+[assembly: AssemblyCopyright("Copyright © {year}")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d9387c3a-db76-4f66-bc75-0a418d7cff12")]
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
